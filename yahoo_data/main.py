8b830ebc342bdc775bf380503eb9fc221d9585e0
Adeola Oladeji <deolaoladeji@gmail.com>
2022-06-13 21:10:12 +0100

    created andcommitted trading bot and lean software

---
+++ yahoo_data/main.py
@@ -0,0 +1,20 @@
+from AlgorithmImports import *
+
+
+class Yahoodata(QCAlgorithm):
+    def Initialize(self):
+        self.SetStartDate(2022, 6, 10)  # Set Start Date
+        self.SetEndDate(2022, 6, 11)  # Set End Date
+        self.SetCash(100000)  # Set Strategy Cash
+        self.AddData(Yahoodata,"SPY", Resolution.Minute).Symbol
+
+    def OnData(self, data):
+        """OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
+            Arguments:
+                data: Slice object keyed by symbol containing the stock data
+        """
+        if not self.Portfolio.Invested:
+            self.SetHoldings("SPY", 1)
+            self.Debug("Purchased Stock")
+        
+        self.Debug(f"{self.symbol.Value} - {self.Time}: Close={data[self.symbol].Close}")
